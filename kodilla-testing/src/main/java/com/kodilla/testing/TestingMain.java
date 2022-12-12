e69ae6115627695d6b1bad556c009c3f887b001a
Michal Semerylo <msemerylo@hotmail.com>
2022-12-12 17:08:25 +0100

    6.2 Calculator test kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+
+            if (result.equals("theForumUser")) {
+                System.out.println("test OK");
+            } else {
+                System.out.println("Error!");
+            }
+        }
+        Calculator calculator = new Calculator();
+
+        Integer result2 = calculator.getCalculatorAdd();
+        if (result2.equals(13)) {
+            System.out.println("Calculator add test: OK");
+        } else {
+            System.out.println("Calculator add test: Error!");
+        }
+        System.out.println(result2);
+
+        Integer result3 = calculator.getCalculatorSubstrack();
+        if (result3.equals(4)) {
+            System.out.println("Calculator substrack test : OK");
+        } else {
+            System.out.println("Calculator sunstrack test : Error!");
+        }
+
+        System.out.println(result3);
     }
 }
+
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int add(int a, int b){
+        return a+b;
+    }
+    int substract(int a, int b){
+        return a-b;
+    }
+    public Integer getCalculatorAdd(){
+        return add(6,7);
+    }
+    public Integer getCalculatorSubstrack(){
+        return substract(8,4);
+    }
+
+
+
+}
